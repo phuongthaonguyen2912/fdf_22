4a8b9e2d7818ca85bb4c3f862f958a91d905687d
phuongthaonguyen2912 <phuongthaonguyen2912@gmail.com>
2016-11-02 15:26:33 +0700

    show-products

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def show
     if valid_page?
+      @products = Product.all
+      @categories = Category.all
       render template: "static_pages/#{params[:page]}"
     else
       render file: "public/404.html", status: :not_found
@@ -12,4 +14,5 @@ class StaticPagesController < ApplicationController
     File.exist? Pathname.new Rails.root + 
      "app/views/static_pages/#{params[:page]}.html.erb"
   end
+
 end
