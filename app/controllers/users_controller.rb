c5c62da9d6187088b10ff771f2cfeb4dc1e94c98
NguyenDinhDuy <nguyendinhduy2864@gmail.com>
2016-11-02 13:23:32 +0700

    Login-Logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: :show
+
+  def show
+
+  end
+
   def new
     @user = User.new
   end
@@ -16,5 +22,13 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    if @user.blank?
+      flash[:danger] = t "flash.find_user_fail"
+      redirect_to root_url
+    end
   end
 end
